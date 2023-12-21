e88518786a52f2323d1384e8ec4122a91df81f1f
rahgirrafi <rahgirrafi@gmail.com>
2023-12-21 19:32:26 +0600

    Conditions and loops

---
+++ conditionals.py
@@ -0,0 +1,21 @@
+#program to detect odd number and even number
+
+number = int(input())
+
+#odd
+
+if number%2 == 0:
+    print('even') 
+    print('We printed the even number') 
+
+else:
+    print('odd')
+    print(number*2)
+
+print('End of Program')
+
+
+
+#console
+#scope
+#indentation ;  TAB key
---
+++ loop.py
@@ -0,0 +1,14 @@
+#for
+#while
+
+number = 1
+
+while number < 1000:
+    print('sorry')
+    #print(f'number value before assignment {number}')
+    number = number +1 #increment 
+    #print(f'number value after assignment {number}')
+
+    #print()
+
+# = sign is called assignment operator
